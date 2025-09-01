7a4cb57c4d3884ebeb72aa187e70fd043402be27
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2025-09-01 13:41:02 -0500

    chore: remove duplicate codegen.py from project root

--- codegen.py
+++
@@ -1,9 +0,0 @@
-class CodeBuilder:
-    def __init__(self) -> None:
-        self._parts: list[str] = []
-
-    def add_line(self, line: str) -> None:
-        self._parts.append(line)
-
-    def build(self) -> str:
-        return "\n".join(self._parts)
